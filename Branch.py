dba8eb4d3304944925bd6881a6d043f8d33eaed0
Jay Kumpavat <a3218239@adelaide.edu.au>
2026-08-31 15:25:46 +0930

    testing the class

--- Branch.py
+++ Branch.py
@@ -1,10 +1,10 @@
 class Branch:
-    def __init__(self, branch_number, branch_name, branch_suburb, branch_phonenumber, branch_status = "closed") -> None:
+    def __init__(self, branch_number: str, branch_name: str, branch_suburb: str, branch_phonenumber) -> None:
         self.b_number = branch_number
         self.b_name = branch_name
         self.b_suburb = branch_suburb
         self.b_phoneno = branch_phonenumber
-        self.b_status = branch_status
+        self.b_status = "Closed"
 
     def open_branch(self):
         if self.b_status == "Closed":
@@ -21,7 +21,7 @@ class Branch:
             return f"Branch is {self.b_status}, therefore it cannot be Closed"
 
     def upd_branch_no(self, new_number: int):
-        if len(str(new_number)) == 10:
+        if len(str(new_number)) == 9:
             self.b_phoneno = new_number
             return f"The Branch phone number is changed to: {self.b_phoneno}"
         else:
@@ -29,5 +29,5 @@ class Branch:
 
     def feedback(self, branch_feedback: str):
         print(f"Feedback: {branch_feedback}")
-        
+
     
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from Transactions import Transaction
 from Client import Client
 from Accounts import Account
+from Branch import Branch
 
 
 
@@ -14,4 +15,15 @@ Account2 = Account(1102, "Savings", 500)
 transfering = Transaction(1, "money_transfer", 10000, "Moving Money to savings")
 #transfering.transfering(1, Account1, Account2, 500, 'Moving money to savings', 'pending')
 transfering.withdrawl(Account1, 1000)
+
+branch1 = Branch("1","branch 1", "Enfield", 419422184)
+branch2 = Branch("2", "branch2", "sailsbury", 592833148)
+print(branch1.b_status)
+print(branch2.b_status)
+
+branch1.open_branch()
+print(branch1.b_status)
+print(branch2.b_status)
+
+
 
