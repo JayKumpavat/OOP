28d7736c3cd016c7c26148a17d8bb91540621910
Jay Kumpavat <a3218239@adelaide.edu.au>
2026-09-02 14:25:31 +0930

    changed all branch attributes to private

--- BankingSystem/Branch.py
+++ BankingSystem/Branch.py
@@ -1,29 +1,29 @@
 class Branch:
     def __init__(self, branch_number: int, branch_name: str, branch_suburb: str, branch_phonenumber: int) -> None:
-        self.b_number = branch_number
-        self.b_name = branch_name
-        self.b_suburb = branch_suburb
-        self.b_phoneno = branch_phonenumber
+        self.__b_number = branch_number
+        self.__b_name = branch_name
+        self.__b_suburb = branch_suburb
+        self.__b_phoneno = branch_phonenumber
         self.b_status = "Closed"
 
     def open_branch(self):
         if self.b_status == "Closed":
             self.b_status = "Opened"
-            print(f"{self.b_name} is now {self.b_status}")
+            print(f"{self.__b_name} is now {self.b_status}")
         else:
-            print(f"{self.b_name} is {self.b_status}, therefore it cannot be Opened")
+            print(f"{self.__b_name} is {self.b_status}, therefore it cannot be Opened")
 
     def closed_branch(self):
         if self.b_status == "Opened":
             self.b_status = "Closed"
-            print(f"{self.b_name} is now {self.b_status}")
+            print(f"{self.__b_name} is now {self.b_status}")
         else:
-            print(f"{self.b_name} is {self.b_status}, therefore it cannot be Closed")
+            print(f"{self.__b_name} is {self.b_status}, therefore it cannot be Closed")
 
     def set_upd_branch_no(self, new_number: int):
         if len(str(new_number)) == 9:
-            self.b_phoneno = new_number
-            print(f"The Branch phone number is changed to: {self.b_phoneno}")
+            self.__b_phoneno = new_number
+            print(f"The Branch phone number is changed to: {self.__b_phoneno}")
         else:
             print(f"Please enter a valid 10 digit phone number")
 
@@ -31,24 +31,24 @@ class Branch:
         print(f"Feedback: {branch_feedback}")
 
     def __str__(self):
-        print(f"The branch number is {self.b_number}, and its name is {self.b_name}. Its located in {self.b_suburb}")
-        print(f"its phone number is {self.b_phoneno} and its current status is {self.b_status}")
+        print(f"The branch number is {self.__b_number}, and its name is {self.__b_name}. Its located in {self.__b_suburb}")
+        print(f"its phone number is {self.__b_phoneno} and its current status is {self.b_status}")
 
     def set_change_branch_name(self, new_branch_name):
-        self.b_name = new_branch_name
-        return f"branch name has been changed to {self.b_name}"
+        self.__b_name = new_branch_name
+        return f"branch name has been changed to {self.__b_name}"
     
     def set_change_branch_suburb(self, new_suburb):
-        self.b_suburb = new_suburb
-        return f"branch {self.b_number} suburb has been changed to {self.b_suburb}"
+        self.__b_suburb = new_suburb
+        return f"branch {self.__b_number} suburb has been changed to {self.__b_suburb}"
     
 
 
     def __repr__(self):
-        print(f"Branch Number: {self.b_number}\n\
-              Name: {self.b_name}\n\
-              Location: {self.b_suburb}\n\
-              Phone number: {self.b_phoneno}\n\
+        print(f"Branch Number: {self.__b_number}\n\
+              Name: {self.__b_name}\n\
+              Location: {self.__b_suburb}\n\
+              Phone number: {self.__b_phoneno}\n\
               Current Status: {self.b_status}")
     
 
