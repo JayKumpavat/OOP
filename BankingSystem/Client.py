4ecf003371b732a081506e60d111a840a8fa7de8
Jay Kumpavat <a3218239@adelaide.edu.au>
2026-09-02 15:02:40 +0930

    adding comments to the functions in client module

--- BankingSystem/Client.py
+++ BankingSystem/Client.py
@@ -9,11 +9,13 @@ class Client:
         self.__client_id = client_id
         self.__client_no = client_no
 
+# Displays client information (name, client_id, client_phone_number)
     def display_client_info(self):
         print("Client ID:", self.__client_id)
         print("Client Name:", self.__client_name)
         print("Client Number:", self.__client_no)
 
+#This function allows client to update their phone number
     def set_change_client_number(self, new_number):
         if isinstance(new_number, int) and len(str(new_number)) == 9:
             self.__client_no = new_number
@@ -21,6 +23,7 @@ class Client:
         else:
             print(f"phone number must have 9 numbers (not including 0)")
 
+#Allows bank to change their name
     def set_client_name(self, new_name):
         if isinstance(new_name, str):
             self.__client_name = new_name
@@ -28,6 +31,7 @@ class Client:
         else:
             print(f"The client name cannot have numbers or symbols, please try again")
 
+#Allows bank to change client_id
     def set_client_id(self, new_id):
         if isinstance(new_id, int) and len(str(new_id)) <= 9:
             self.__client_id = new_id
@@ -35,10 +39,11 @@ class Client:
         else:
             print(f"the new client_id must be between 1 - 9 digits")
 
-
+#str for showing client information in readable form
     def __str__(self):
         print(f"Client's Id is {self.__client_id} and client name is {self.__client_name} and their phone number is {self.__client_no}")
 
+#repr to just show important information
     def __repr__(self):
         print(f"Client Id: {self.__client_id}\n\
             Client Name: {self.__client_name}\n\
