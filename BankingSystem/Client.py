a1a8e1f3c3061ef0363c3f82ac9fcd07d6d05ca0
Jay Kumpavat <a3218239@adelaide.edu.au>
2026-09-02 14:02:17 +0930

    changed client_id and client_no from public to private

--- BankingSystem/Client.py
+++ BankingSystem/Client.py
@@ -6,17 +6,17 @@ class Client:
         client_no,
     ):
         self.client_name = client_name
-        self.client_id = client_id
-        self.client_no = client_no
+        self.__client_id = client_id
+        self.__client_no = client_no
 
     def display_client_info(self):
-        print("Client ID:", self.client_id)
+        print("Client ID:", self.__client_id)
         print("Client Name:", self.client_name)
-        print("Client Number:", self.client_no)
+        print("Client Number:", self.__client_no)
 
     def change_client_number(self, new_number):
-        self.client_no = new_number
-        print("Client number updated to:", self.client_no)
+        self.__client_no = new_number
+        print("Client number updated to:", self.__client_no)
 
     def set_client_name(self, new_name):
         self.client_name = new_name
@@ -24,12 +24,12 @@ class Client:
 
 
     def __str__(self):
-        print(f"Client's Id is {self.client_id} and client name is {self.client_name} and their phone number is {self.client_no}")
+        print(f"Client's Id is {self.__client_id} and client name is {self.client_name} and their phone number is {self.__client_no}")
 
     def __repr__(self):
-        print(f"Client Id: {self.client_id}\n\
+        print(f"Client Id: {self.__client_id}\n\
             Client Name: {self.client_name}\n\
-            Client Phone Number: {self.client_no}")
+            Client Phone Number: {self.__client_no}")
 
 
 
