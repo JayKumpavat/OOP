2c4cfa57f66bd726b4e1a227769901e8ebe73b5b
Jay Kumpavat <a3218239@adelaide.edu.au>
2026-09-03 23:27:52 +0930

    going defult main menu

---
+++ main.py
@@ -0,0 +1,7 @@
+from BankingSystem.Accounts import Account
+from BankingSystem.Client import Client
+from BankingSystem.Branch import Branch
+from BankingSystem.Transactions import Transaction
+
+# Setup 
+
